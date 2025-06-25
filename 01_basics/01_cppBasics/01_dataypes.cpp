187aeaf83d4b8a76c2f2dcf08b5ef4970b35f162
Shivam Raj <rajshivam5984@gmail.com>
2025-06-25 16:29:32 +0530

    langBasics

---
+++ 01_basics/01_cppBasics/01_dataypes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // input-output => cin and cout
+    // endl, /n
+
+    // DataTypes
+    // int, long, long long
+    int x1 = 10;
+    long x2 = 15;
+    long long x3 = 1400000666;
+
+    // float, double
+    float x4 = 15.5;
+    double x5 = 415.61;
+
+    // string and getline
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1 << " " << s2;
+
+    string str;
+    getline(cin, str);
+    cout << str;
+
+    // char (256 characters)
+    char ch = 'g';
+
+    return 0;
+}
