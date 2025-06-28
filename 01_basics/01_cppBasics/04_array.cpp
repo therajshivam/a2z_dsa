6f6b1dce5e86fbb45bf9cee3a164c5b0d3dca90c
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 13:38:42 +0530

    arrayBasics

---
+++ 01_basics/01_cppBasics/04_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] ;
+
+    cout << arr[3];
+    
+    // 2D
+     
+
+    return 0;
+}
