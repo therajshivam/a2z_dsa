9f11b904d1e3cf0f4737730a4d7cd6e8e7e5be48
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 13:48:23 +0530

    arrayBasics

--- 01_basics/01_cppBasics/04_array.cpp
+++ 01_basics/01_cppBasics/04_array.cpp
@@ -7,8 +7,15 @@ int main(){
     cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] ;
 
     cout << arr[3];
-    
+
     // 2D
+    // row, column
+    int arr2 [3][5];
+
+    arr2 [1][3] = 4;
+    cout << arr2 [1][3];
+    cout << arr2 [1][2]; // gives garbage value
+
      
 
     return 0;
