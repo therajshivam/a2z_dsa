2f22d80eed1ab6a2c4d2199cef99085a29411d94
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 15:33:03 +0530

    functions

--- 01_basics/01_cppBasics/11_valueAndReference.cpp
+++ 01_basics/01_cppBasics/11_valueAndReference.cpp
@@ -32,3 +32,20 @@ using namespace std;
 // }
 
 // pass by reference of an array
+// array always pass by reference by default
+void doSomething(int arr[], int n){
+    arr[0] += 100;
+    cout << "Value inside the function: " << arr[0] << endl;
+}
+int main(){
+    int n = 5;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    doSomething(arr, n);
+
+    cout << "Value inside int main; " << arr[0] << endl;
+    return 0;
+}
