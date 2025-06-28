c51fcbc498fef6cb796a8fc34ac07d55af7e51f3
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 14:39:24 +0530

    functions

--- 01_basics/01_cppBasics/09_function.cpp
+++ 01_basics/01_cppBasics/09_function.cpp
@@ -13,12 +13,25 @@ void printName(string name){
     cout << "Hello " << name << endl;
 }
 
+// take two nums and return its sum 
+// return func
+int sum(int num1, int num2){
+    int num3 = num1 + num2;
+    return num3;
+}
+
 int main(){
     string name = "Shivam";
     printName(name);
 
     string name2 = "Sid";
     printName(name2);
+
+    int num1, num2;
+    cin >> num1 >> num2;
+    int result = sum(num1, num2);
+    cout << result;
+    
 
     return 0;
 }
