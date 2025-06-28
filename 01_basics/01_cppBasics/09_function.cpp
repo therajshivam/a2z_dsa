d32716c2577893cbe0db62a9f15a5179598718f2
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 14:19:44 +0530

    basics

---
+++ 01_basics/01_cppBasics/09_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// functions are set of code which performs something for you
+// functions are used to modularise code
+// functions are used to increase readability
+// functions are used to use same code multiple times
+
+int main(){
+    
+    
+    return 0;
+}
