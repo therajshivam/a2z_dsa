ce990b8d7c474f5cef3723b1aa401ccf102c85ad
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 15:26:46 +0530

    functions

---
+++ 01_basics/01_cppBasics/10_moreFunc.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+    cin >> num1 >> num2;
+    // inbuilt functions
+    int minimum = min(num1, num2);
+    cout << minimum;
+    int maximum = max(num1, num2);
+    cout << maximum;
+
+    return 0;
+}
---
+++ 01_basics/01_cppBasics/11_valueAndReference.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// pass by value = copy sends
+// void doSomething(int num){
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+// }
+// int main(){
+//     int num = 10;
+//     doSomething(num);
+//     cout << num << endl;    
+//     return 0;
+// }
+
+// pass by reference = address sends (&)
+// void doSomething(int &num){
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+// }
+// int main(){
+//     int num = 10;
+//     doSomething(num);
+//     cout << num << endl;    
+//     return 0;
+// }
+
+// pass by reference of an array
