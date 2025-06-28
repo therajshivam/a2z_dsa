4b7f84b1a0bdf0142aa4f10a3cf7fd87f372d84a
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 14:14:30 +0530

    doWhile

---
+++ 01_basics/01_cppBasics/08_doWhileLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // use to execute minimum for one time
+    int i = 2;
+    do {
+        cout << "Hey! " << i << endl;
+        i++;
+    } while (i<=1);
+    cout << i << endl;
+    
+    return 0;
+}
