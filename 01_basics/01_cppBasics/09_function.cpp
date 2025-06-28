4cde30f136e7969416a7fa0bb53d1fc702a59c13
Shivam Raj <rajshivam5984@gmail.com>
2025-06-28 14:30:57 +0530

    functions

--- 01_basics/01_cppBasics/09_function.cpp
+++ 01_basics/01_cppBasics/09_function.cpp
@@ -5,8 +5,20 @@ using namespace std;
 // functions are used to increase readability
 // functions are used to use same code multiple times
 
+// four kind of functions:-
+// void -> does not return anything
+//  return, parameterised, non parameterised
+
+void printName(string name){
+    cout << "Hello " << name << endl;
+}
+
 int main(){
-    
-    
+    string name = "Shivam";
+    printName(name);
+
+    string name2 = "Sid";
+    printName(name2);
+
     return 0;
 }
